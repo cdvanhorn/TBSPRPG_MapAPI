697c138638a5a907838738fbb70122c0e294bc45
cdvanhorn <cdavid.vanhorn@gmail.com>
2020-12-06 21:59:50 -0500

    MapAPI
    Updated my notes.

--- MapApi/Entities/Location.cs
+++ MapApi/Entities/Location.cs
@@ -16,11 +16,17 @@ namespace MapApi.Entities {
     }
 }
 
-//on new game event create entry in database that maps games to adventures
-//and creates an enter_location event
+//final notes
+//Listen for new_game events
+//on new game event, build the aggregrate
+//check if the event id prefixed with this service id is in the list of event ids in the aggregrate
+//convert it to a MapApi.Game object
+//write the game to the database game collection for this service
+//look up the initial location for the given adventure
+//create an enter_location event that contains this service id plus the new_game event id
 
-//we're going to listen for an entered_location event and enter it in to the database
-//that enter location will need the game id
-
-//enter_location ( gameid, adventureid, location_name )
-//entered_location ( gameid, adventureid, location_name )
+//Listen for entered_location event
+//build the game aggregrate
+//check if the entered_location event id prefixed with this service id is in the list of event ids in the aggregrate
+//convert it ot a MapApi.Game object
+//write to the location collection in the map database
